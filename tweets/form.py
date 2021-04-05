1aa8078662f2deeb1f8e90d98a5a2d82eaa98b91
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2021-04-05 20:44:38 +0530

    Error handling with Pure javaScript

--- tweets/form.py
+++ tweets/form.py
@@ -1,13 +1,16 @@
 from django import forms
 from .models import Tweet
 
-MAX_TWEET_LENGTH =240
+MAX_TWEET_LENGTH = 240
+
+
 class Tweetfrom(forms.ModelForm):
     class Meta:
         model = Tweet
-        fields =['content']
+        fields = ['content']
+
     def clean_content(self):
         content = self.cleaned_data.get("content")
-        if (len(content)>MAX_TWEET_LENGTH):
+        if len(content) > MAX_TWEET_LENGTH:
             raise forms.ValidationError("This Tweet Is Way Too Long")
-        return (content)
+        return content
